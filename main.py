c23dc8db8cf84310f1b2f7e287af2c9a23eb7c3f
johnsonco19 <84600778+johnsonco19@users.noreply.github.com>
2023-04-02 20:22:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from translate import Translator
-translator= Translator(to_lang="German")
-translation = translator.translate("Good Morning!")
-print(translation)
+translator_german = Translator(to_lang="German")
+translator_chinese = Translator(to_lang="zh")
+translation1 = translator_german.translate("Good Morning!")
+translation2 = translator_chinese.translate("Good Morning!")
+print(translation1)
+print(translation2)
